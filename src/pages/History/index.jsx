cd0e2f42ca1d14ba0b367203b91f2a3b97023dbb
sudjoao <isudjoao@gmail.com>
2022-03-13 08:06:32 -0300

    Add conditional to check if has something in history

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -4,35 +4,39 @@ import PokemonCard from "../../components/PokemonCard";
 import './styles.css';
 export default function HistoryPage(){
     const [history, setHistory] = useState([]);
-    const [isLoading, setIsloading] = useState(true);
 
     useEffect(()=>{
-        setHistory(JSON.parse(localStorage.getItem("history")));
-    }, [])
+        const historyData = localStorage.getItem("history");
+        if(historyData!= null)
+            setHistory(JSON.parse(historyData));
+    }, []);
 
     return(
         <div className="history-container">
             <NavBar/>
             <h1>Histórico</h1>
             <section className="history-data">
-                {history.map((tradeData, index)=>(
-                    <div key={index}>
-                        <h2>Troca {index+1}</h2>
-                        <div className="trade-info-container">
-                            <div className="trade-pokemons-container">
-                                {tradeData.pokemonListUser1.map((pokemon, id)=>(
-                                <PokemonCard pokemon={pokemon} showDeleteButton={false} key={id}/>
+                {history.length > 0 ? 
+                    history.map((tradeData, index)=>(
+                        <div key={index}>
+                            <h2>Troca {index+1}</h2>
+                            <div className="trade-info-container">
+                                <div className="trade-pokemons-container">
+                                    {tradeData.pokemonListUser1.map((pokemon, id)=>(
+                                    <PokemonCard pokemon={pokemon} showDeleteButton={false} key={id}/>
+                                    ))}
+                                </div>
+                                <h2>VS</h2>
+                                <div className="trade-pokemons-container">
+                                {tradeData.pokemonListUser2.map((pokemon, id)=>(
+                                    <PokemonCard pokemon={pokemon} showDeleteButton={false} key={id}/>
                                 ))}
-                            </div>
-                            <h2>VS</h2>
-                            <div className="trade-pokemons-container">
-                            {tradeData.pokemonListUser2.map((pokemon, id)=>(
-                                <PokemonCard pokemon={pokemon} showDeleteButton={false} key={id}/>
-                            ))}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                )) :
+                <p>Nenhuma troca realizada.</p>
+            }
             </section>
         </div>
     );
